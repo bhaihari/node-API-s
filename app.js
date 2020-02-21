e9b4858adfae7c608c390075a86726003b9d88c8
Hari Gautam Dubey <gautamhari02@gmail.com>
2020-02-21 14:33:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const mysql = require('mysql');
 const app =express();
 
 app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extende:true}));
+app.all("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+
+  });
 
 const db = mysql.createConnection({
     host:'localhost',
@@ -25,7 +33,7 @@ db.connect((err)=>{
 // List of all the product
 
 
-app.get('/Home',(req,res)=>{
+app.get('/Home',(req,res,next)=>{
     db.query('SELECT * FROM books ',(err,rows,fields)=>{
         if(!err){
             console.log(rows);
@@ -46,9 +54,10 @@ app.get('/Home',(req,res)=>{
 
 // List of particular product
 
-app.get('/Home:id',(req,res)=>{
-    let book=req.body;
-    db.query('SELECT * FROM books WHERE BookId="'+book.BookId+'"',[req.params.id],(err,rows,fields)=>{
+app.get('/Home/:id',(req,res,next)=>{
+    let book=req.params;
+    console.log(book);
+    db.query('SELECT * FROM books WHERE BookId=?',[req.params.id],(err,rows,fields)=>{
         if(!err){
             console.log(rows);
             res.send(rows);
@@ -63,50 +72,58 @@ app.get('/Home:id',(req,res)=>{
 //delete 
 
 
-app.delete('/Home:id',(req,res)=>{
+app.delete('/Home/delete/:id',(req,res,next)=>{
 
-    let book = req.body;
-    db.query('DELETE FROM books WHERE BookId="'+book.BookId+'"',[req.params.id],(err,rows,fields)=>{
-        if(!err){
-            
-            res.send('Deleted Successfully');
-        }
-        else
-            console.log(err);
-    });
+    console.log(req.params)
+    db.query('DELETE FROM books WHERE BookId='+ req.params.id);
 
 });
 
 // create
 
-app.post('/Home',(req,res)=>{
-    let book= req.body;
-   
-    db.query('INSERT INTO books values("'+book.BookId+'","'+book.BookName+'","'+book.Amount+'")',(err,rows,fields)=>{
-        if(!err){
-            
-            res.send(JSON.stringify(rows));
-        }
-        else
-            console.log(err);
-    });
+
+
+app.post('/Home',(req,res,next)=>{
+        
+    console.log(req.body.name);
+    db.query('INSERT INTO books values("'+req.body.id+'","'+req.body.name+'","'+req.body.amount+'")');
 
 });
 
 
+
 // update
 
-app.put('/Home',(req,res)=>{
+app.put('/Home',(req,res,next)=>{
     let book= req.body;
-
-    db.query('UPDATE books set Amount ="'+book.Amount+'" WHERE BookId="'+book.BookId+'"',(err,rows,fields)=>{
-
+    console.log(req.body);
+    db.query('UPDATE books set Amount = ? , BookName= ? WHERE BookId= ? ',[req.body.amount,req.body.name,req.body.id],(err,rows,fields)=>{
+        if(err) throw err;
         res.send(rows);
         console.log(rows);
     });
 });
 
 
+
+
+//delete 
+
+
+app.delete('/Home/:id',(req,res,next)=>{
+    console.log(req.body.id);
+     
+        db.query('DELETE FROM books WHERE BookId="'+req.body.id+'"',[req.params.id],(err,rows,fields)=>{
+            if(!err){
+                
+                res.send('Deleted Successfully');
+                console.log("executed");
+            }
+            else
+                console.log(err);
+        });
+    
+    });
 
 
 
